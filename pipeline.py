c4cc170db29ebb1a3d26e6169efabb4abc4600f5
Pavel K <pk@1702.ru>
2023-01-09 18:14:09 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -26,9 +26,11 @@ files =    ['circuits.csv',
 remote_url = 'http://ergast.com/downloads/f1db_csv.zip'
 local_file = 'f1db_csv.zip'
 request.urlretrieve(remote_url, local_file)
+print('Download of remote file finished...')
 
 with ZipFile('f1db_csv.zip', 'r') as zipObj:
    zipObj.extractall()
+print('Unpacking of the downloaded file finished...')
 
 with open('db_user.txt', 'r', encoding='utf-8') as fp:
     db_user = fp.read().rstrip()
@@ -43,4 +45,6 @@ conn_url = 'postgresql://' + db_user + ':' + db_user_password + '@' + hostname
 conn = create_engine(conn_url)
 
 for i in files:
+    print(f'Loadinging of file "{i}" into DB...')
     csv2sql(i)
+    print(f'Loading of file "{i}" into DB finished.')
