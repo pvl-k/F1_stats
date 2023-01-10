f309b88ec6f48fc1d669bca0f24ad546bc9aea98
Pavel K <pk@1702.ru>
2023-01-10 20:53:10 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,111 +1,43 @@
+CREATE TABLE constructors (
+	constructorId smallint NOT NULL,
+	constructorRef varchar(50) NOT NULL,
+	name varchar(50) NOT NULL,
+	nationality varchar(50) NOT NULL,
+	url varchar(255) NOT NULL,
+	PRIMARY KEY (constructorId)
+);
+
 CREATE TABLE circuits (
-	circuitId int NOT NULL,
-	circuitRef varchar(30) NOT NULL,
-	name varchar(100) NOT NULL,
-	location varchar(30) NOT NULL,
-	country varchar(30) NOT NULL,
-	lat DOUBLE PRECISION NOT NULL,
-	lng DOUBLE PRECISION NOT NULL,
+	circuitId smallint NOT NULL,
+	circuitRef varchar(50) NOT NULL,
+	name varchar(255) NOT NULL,
+	location varchar(50) NOT NULL,
+	country varchar(50) NOT NULL,
+	lat float NOT NULL,
+	lng float NOT NULL,
 	alt smallint,
 	url varchar(255) NOT NULL,
 	PRIMARY KEY (circuitId)
 );
 
-
-CREATE TABLE constructor_results (
-	constructorResultsId smallint NOT NULL,
-	raceId smallint NOT NULL,
-	constructorId smallint NOT NULL,
-	points smallint NOT NULL,
-	status varchar(10),
-	PRIMARY KEY (constructorResultsId)
-);
-
-
-CREATE TABLE constructor_standings (
-	constructorStandingsId smallint NOT NULL,
-	raceId smallint NOT NULL,
-	constructorId smallint NOT NULL,
-	points smallint NOT NULL,
-	position smallint NOT NULL,
-	positionText varchar(3) NOT NULL,
-	wins smallint NOT NULL,
-	PRIMARY KEY (constructorStandingsId)
-);
-
-
-CREATE TABLE constructors (
-	constructorId smallint NOT NULL,
-	constructorRef varchar(20) NOT NULL,
-	name varchar(50) NOT NULL,
-	nationality varchar(15) NOT NULL,
-	url varchar(255) NOT NULL,
-	PRIMARY KEY (constructorId)
-);
-
-
-CREATE TABLE driver_standings (
-	driverStandingsId int NOT NULL,
-	raceId smallint NOT NULL,
-	driverId smallint NOT NULL,
-	points smallint NOT NULL,
-	position smallint NOT NULL,
-	positionText varchar(3) NOT NULL,
-	wins smallint NOT NULL,
-	PRIMARY KEY (driverStandingsId)
-);
-
-
 CREATE TABLE drivers (
 	driverId smallint NOT NULL,
-	driverRef varchar(20) NOT NULL,
+	driverRef varchar(50) NOT NULL,
 	number smallint,
-	code varchar(4),
-	forename varchar(20) NOT NULL,
-	surname varchar(20) NOT NULL,
+	code varchar(10),
+	forename varchar(50) NOT NULL,
+	surname varchar(50)  NOT NULL,
 	dob DATE NOT NULL,
-	nationality varchar(15) NOT NULL,
+	nationality varchar(50) NOT NULL,
 	url varchar(255) NOT NULL,
 	PRIMARY KEY (driverId)
 );
 
-
-CREATE TABLE lap_times (
-	raceId smallint NOT NULL,
-	driverId smallint NOT NULL,
-	lap smallint NOT NULL,
-	position smallint NOT NULL,
-	time varchar(10) NOT NULL,
-	milliseconds int NOT NULL,
-	PRIMARY KEY (raceId)
+CREATE TABLE seasons (
+	year smallint NOT NULL,
+	url varchar(255) NOT NULL,
+	PRIMARY KEY (year)
 );
-
-
-CREATE TABLE pit_stops (
-	raceId smallint NOT NULL,
-	driverId smallint NOT NULL,
-	stop smallint NOT NULL,
-	lap smallint NOT NULL,
-	time TIME(0) NOT NULL,
-	duration varchar(10) NOT NULL,
-	milliseconds int NOT NULL,
-	PRIMARY KEY (raceId)
-);
-
-
-CREATE TABLE qualifying (
-	qualifyId smallint NOT NULL,
-	raceId smallint NOT NULL,
-	driverId smallint NOT NULL,
-	constructorId smallint NOT NULL,
-	number smallint NOT NULL,
-	position smallint NOT NULL,
-	q1 varchar(10),
-	q2 varchar(10),
-	q3 varchar(10),
-	PRIMARY KEY (qualifyId)
-);
-
 
 CREATE TABLE races (
 	raceId smallint NOT NULL,
@@ -129,6 +61,70 @@ CREATE TABLE races (
 	PRIMARY KEY (raceId)
 );
 
+CREATE TABLE constructor_results (
+	constructorResultsId smallint NOT NULL,
+	raceId smallint NOT NULL,
+	constructorId smallint NOT NULL,
+	points float NOT NULL,
+	status varchar(10),
+	PRIMARY KEY (constructorResultsId)
+);
+
+CREATE TABLE constructor_standings (
+	constructorStandingsId smallint NOT NULL,
+	raceId smallint NOT NULL,
+	constructorId smallint NOT NULL,
+	points float NOT NULL,
+	position smallint NOT NULL,
+	positionText varchar(3) NOT NULL,
+	wins smallint NOT NULL,
+	PRIMARY KEY (constructorStandingsId)
+);
+
+CREATE TABLE driver_standings (
+	driverStandingsId int NOT NULL,
+	raceId smallint NOT NULL,
+	driverId smallint NOT NULL,
+	points float NOT NULL,
+	position smallint NOT NULL,
+	positionText varchar(255) NOT NULL,
+	wins smallint NOT NULL,
+	PRIMARY KEY (driverStandingsId)
+);
+
+CREATE TABLE lap_times (
+	raceId smallint NOT NULL,
+	driverId smallint NOT NULL,
+	lap smallint NOT NULL,
+	position smallint NOT NULL,
+	time varchar(10) NOT NULL,
+	milliseconds int NOT NULL,
+	PRIMARY KEY (raceId, driverId, lap)
+);
+
+CREATE TABLE pit_stops (
+	raceId smallint NOT NULL,
+	driverId smallint NOT NULL,
+	stop smallint NOT NULL,
+	lap smallint NOT NULL,
+	time TIME(0) NOT NULL,
+	duration varchar(10) NOT NULL,
+	milliseconds int NOT NULL,
+	PRIMARY KEY (raceId, driverId, stop)
+);
+
+CREATE TABLE qualifying (
+	qualifyId smallint NOT NULL,
+	raceId smallint NOT NULL,
+	driverId smallint NOT NULL,
+	constructorId smallint NOT NULL,
+	number smallint NOT NULL,
+	position smallint NOT NULL,
+	q1 varchar(10),
+	q2 varchar(10),
+	q3 varchar(10),
+	PRIMARY KEY (qualifyId)
+);
 
 CREATE TABLE results (
 	resultId int NOT NULL,
@@ -140,7 +136,7 @@ CREATE TABLE results (
 	position smallint,
 	positionText varchar(3) NOT NULL,
 	positionOrder smallint NOT NULL,
-	points smallint NOT NULL,
+	points float NOT NULL,
 	laps smallint NOT NULL,
 	time varchar(15),
 	milliseconds int,
@@ -152,14 +148,6 @@ CREATE TABLE results (
 	PRIMARY KEY (resultId)
 );
 
-
-CREATE TABLE seasons (
-	year smallint NOT NULL,
-	url varchar(255) NOT NULL,
-	PRIMARY KEY (year)
-);
-
-
 CREATE TABLE sprint_results (
 	resultId int NOT NULL,
 	raceId smallint NOT NULL,
@@ -170,7 +158,7 @@ CREATE TABLE sprint_results (
 	position smallint,
 	positionText varchar(3) NOT NULL,
 	positionOrder smallint NOT NULL,
-	points smallint NOT NULL,
+	points float NOT NULL,
 	laps smallint NOT NULL,
 	time varchar(15),
 	milliseconds int,
@@ -180,49 +168,8 @@ CREATE TABLE sprint_results (
 	PRIMARY KEY (resultId)
 );
 
-
 CREATE TABLE status (
 	statusId smallint NOT NULL,
 	status varchar(255) NOT NULL,
 	PRIMARY KEY (statusId)
 );
-
-ALTER TABLE constructor_results ADD CONSTRAINT constructor_results_fk0 FOREIGN KEY (raceId) REFERENCES races(raceId);
-
-ALTER TABLE constructor_results ADD CONSTRAINT constructor_results_fk1 FOREIGN KEY (constructorId) REFERENCES constructors(constructorId);
-
-ALTER TABLE constructor_standings ADD CONSTRAINT constructor_standings_fk0 FOREIGN KEY (raceId) REFERENCES races(raceId);
-
-ALTER TABLE constructor_standings ADD CONSTRAINT constructor_standings_fk1 FOREIGN KEY (constructorId) REFERENCES constructors(constructorId);
-
-ALTER TABLE driver_standings ADD CONSTRAINT driver_standings_fk0 FOREIGN KEY (raceId) REFERENCES races(raceId);
-
-ALTER TABLE driver_standings ADD CONSTRAINT driver_standings_fk1 FOREIGN KEY (driverId) REFERENCES drivers(driverId);
-
-ALTER TABLE lap_times ADD CONSTRAINT lap_times_fk0 FOREIGN KEY (driverId) REFERENCES drivers(driverId);
-
-ALTER TABLE pit_stops ADD CONSTRAINT pit_stops_fk0 FOREIGN KEY (driverId) REFERENCES drivers(driverId);
-
-ALTER TABLE qualifying ADD CONSTRAINT qualifying_fk0 FOREIGN KEY (raceId) REFERENCES races(raceId);
-
-ALTER TABLE qualifying ADD CONSTRAINT qualifying_fk1 FOREIGN KEY (driverId) REFERENCES drivers(driverId);
-
-ALTER TABLE qualifying ADD CONSTRAINT qualifying_fk2 FOREIGN KEY (constructorId) REFERENCES constructors(constructorId);
-
-ALTER TABLE races ADD CONSTRAINT races_fk0 FOREIGN KEY (circuitId) REFERENCES circuits(circuitId);
-
-ALTER TABLE results ADD CONSTRAINT results_fk0 FOREIGN KEY (raceId) REFERENCES races(raceId);
-
-ALTER TABLE results ADD CONSTRAINT results_fk1 FOREIGN KEY (driverId) REFERENCES drivers(driverId);
-
-ALTER TABLE results ADD CONSTRAINT results_fk2 FOREIGN KEY (constructorId) REFERENCES constructors(constructorId);
-
-ALTER TABLE results ADD CONSTRAINT results_fk3 FOREIGN KEY (statusId) REFERENCES status(statusId);
-
-ALTER TABLE sprint_results ADD CONSTRAINT sprint_results_fk0 FOREIGN KEY (raceId) REFERENCES races(raceId);
-
-ALTER TABLE sprint_results ADD CONSTRAINT sprint_results_fk1 FOREIGN KEY (driverId) REFERENCES drivers(driverId);
-
-ALTER TABLE sprint_results ADD CONSTRAINT sprint_results_fk2 FOREIGN KEY (constructorId) REFERENCES constructors(constructorId);
-
-ALTER TABLE sprint_results ADD CONSTRAINT sprint_results_fk3 FOREIGN KEY (statusId) REFERENCES status(statusId);
