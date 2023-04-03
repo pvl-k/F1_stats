f7db51f0445eb226b57e24c0e379b0924266c92d
Pavel K <pk@1702.ru>
2023-04-03 11:55:55 +0300

    Update csv2clickhouse.py

--- v4_Clickhouse/csv2clickhouse.py
+++ v4_Clickhouse/csv2clickhouse.py
@@ -1,7 +1,17 @@
 import glob
+import pandas as pd
 # import os
 
+# Enter path 
 dirpath = '/Users/pk/Downloads/data/'
+
 # os.system(f'cd {dirpath}')
-fileslist = [filename.rsplit('/', 1)[1][:-4] for filename in glob.glob(dirpath + '*.csv')]
+fileslist = [filename.rsplit('/', 1)[1] for filename in glob.glob(dirpath + '*.csv')]
 print(fileslist)
+
+for filename in fileslist:
+    df = pd.read_csv(dirpath + filename)
+    columnName = list(df.columns.values)
+    # print(columnName)
+    # print(df)
+    # print(filename[:-4])
