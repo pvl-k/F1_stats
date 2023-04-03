4c4f68364f5312d0465aaeacdbc68d0eaa7d7774
Pavel K <pk@1702.ru>
2023-04-03 12:32:24 +0300

    Update csv2clickhouse.py

--- v4_Clickhouse/csv2clickhouse.py
+++ v4_Clickhouse/csv2clickhouse.py
@@ -12,6 +12,44 @@ print(fileslist)
 for filename in fileslist:
     df = pd.read_csv(dirpath + filename)
     columnName = list(df.columns.values)
-    # print(columnName)
+    columnType = [str(elem) for elem in df.dtypes]
+    
+    for i in range(len(columnType)):
+        if columnType[i] == 'object':
+            columnType[i] = 'String'
+        elif columnType[i] == 'int64':
+            columnType[i] = 'UInt64'
+        elif columnType[i] == 'int32':
+            columnType[i] = 'UInt32'
+        elif columnType[i] == 'float64':
+            columnType[i] = 'Float64'
+        elif columnType[i] == 'float32':
+            columnType[i] = 'Float32'    
+    
+    columnNameTypes = dict(zip(columnName, columnType))
+    
+    for k, v in columnNameTypes.items():
+        
+        
+        
+        print(k, v)
+    
+    
+    
+    
+    
+    # print(columnNameTypes)
     # print(df)
     # print(filename[:-4])
+    
+    # dataList = []
+    # for x in dataTypes:
+    #     if(x == 'int64'):
+    #         dataList.append('int')
+    #     elif (x == 'float64'):
+    #         dataList.append('float')
+    #     elif (x == 'bool'):
+    #         dataList.append('boolean')
+    #     else:
+    #         dataList.append('varchar')
+    # return dataListcolumnDataType = getColumnDtypes(resultDF.dtypes)
